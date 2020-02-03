1a0c42290c84d320a885120dae0229d6b9d7e4ec
Mathias <juniormathiias@gmail.com>
2020-02-03 21:30:13 +0000

    Tabuada bem Simples

---
+++ script.js
@@ -0,0 +1,22 @@
+function tabuada(){
+    var num = document.getElementById('numero')
+    var tab = document.getElementById(`seltab`)
+    
+    if(num.value.length == 0){
+        alert("digite o numero")
+    }else{
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ``
+        while(c <= 10){
+            var intem = document.createElement('option')
+            intem.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(intem)
+            c++
+        }
+    }
+
+
+
+
+}
