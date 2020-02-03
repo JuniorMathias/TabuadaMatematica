e1bf6b820cf535047985ae8ab197d37173ef5940
Mathias <juniormathiias@gmail.com>
2020-02-03 22:03:19 +0000

    Tabuada Finalizada e com o testes feitos

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 function tabuada(){
-    var num = document.getElementById('numero')
-    var tab = document.getElementById(`seltab`)
-    
-    if(num.value.length == 0){
-        alert("digite o numero")
-    }else{
-        var n = Number(num.value)
-        var c = 1
-        tab.innerHTML = ``
-        while(c <= 10){
-            var intem = document.createElement('option')
-            intem.text = `${n} x ${c} = ${n*c}`
-            tab.appendChild(intem)
-            c++
-        }
+    var num = document.getElementById("numero")
+    var tab = document.getElementById("seltab")
+if( num.value.length == 0){
+    alert("Campo em Branco")
+}else{
+    var n = Number(num.value)
+    var c = 1
+    tab.innerHTML = ``
+    while(c <= 10){
+        var intem = document.createElement('option')
+        intem.text = `${n} x ${c} = ${n*c}`
+        tab.appendChild(intem)
+        c++
     }
-
-
-
-
+}
 }
